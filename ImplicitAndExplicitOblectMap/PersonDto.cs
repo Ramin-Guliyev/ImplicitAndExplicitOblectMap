7987f18792e6544de2d5bc69989198f06d3829f8
Ramin-Guliyev <ra1000ishere@outlook.com>
2021-09-07 20:49:06 +0400

    Add project files.

---
+++ ImplicitAndExplicitOblectMap/PersonDto.cs
@@ -0,0 +1,29 @@
+﻿namespace ImplicitAndExplicitOblectMap
+{
+    public class PersonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+
+        public static implicit operator PersonDto(Person person)
+        {
+            return new PersonDto
+            {
+                Id = person.Id,
+                Name = person.Name,
+                Lastname = person.Lastname
+            };
+        }
+        public static implicit operator Person(PersonDto person)
+        {
+            return new Person
+            {
+                Id = person.Id,
+                Name = person.Name,
+                Lastname = person.Lastname,
+                IsActive=false
+            };
+        }
+    }
+}
---
+++ ImplicitAndExplicitOblectMap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ImplicitAndExplicitOblectMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personDto =new PersonDto
+            {
+                Id = 1,
+                Name = "Ramin",
+                Lastname = "Guliyev"
+            };
+
+            Console.WriteLine(personDto.Name);
+        }
+    }
+}
